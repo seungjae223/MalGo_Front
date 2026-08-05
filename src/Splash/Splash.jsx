df7b87bee8cebae6304994706d260161afb10fd7
ttochi <joyfull223@naver.com>
2026-08-06 00:51:31 +0900

    첫 커밋

---
+++ src/Splash/Splash.jsx
@@ -0,0 +1,58 @@
+import "./Splash.css";
+
+import puzzleKr from "../img/퍼즐1.svg";
+import puzzleEn from "../img/퍼즐2.svg";
+import startButton from "../img/시작하기.png";
+import languageIcon from "../img/언어.png";
+
+function Splash() {
+  const handleStart = () => {
+    // 시작하기 버튼 클릭 시 로그인 페이지로 이동
+    window.location.href = "/login";
+  };
+
+  return (
+    <main className="splash">
+      {/* 우측 상단 언어 아이콘 */}
+      <img
+        className="splash__language"
+        src={languageIcon}
+        alt=""
+        aria-hidden="true"
+        draggable="false"
+      />
+
+      {/* 상단 KR 퍼즐 */}
+      <img
+        className="splash__puzzle splash__puzzle--kr"
+        src={puzzleKr}
+        alt="한국어"
+        draggable="false"
+      />
+
+      {/* 하단 EN 퍼즐 */}
+      <img
+        className="splash__puzzle splash__puzzle--en"
+        src={puzzleEn}
+        alt="영어"
+        draggable="false"
+      />
+
+      {/* 시작하기 버튼 */}
+      <button
+        className="splash__start-button"
+        type="button"
+        onClick={handleStart}
+        aria-label="시작하기"
+      >
+        <img
+          src={startButton}
+          alt=""
+          draggable="false"
+        />
+      </button>
+    </main>
+  );
+}
+
+export default Splash;
